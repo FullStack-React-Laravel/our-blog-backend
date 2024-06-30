71c86e8132b5db8a502c2eada7e8d588e5343faf
saeed-ayman <saeed20359@gmail.com>
2024-06-30 12:16:58 +0300

    edit access

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::resource("/users", \App\Http\Controllers\UserController::class);
 Route::resource("/categories", \App\Http\Controllers\CategoryController::class);
