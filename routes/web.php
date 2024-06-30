08778ab05f22c7ae783e477cab9bd0c8da38ab65
saeed-ayman <saeed20359@gmail.com>
2024-06-30 12:27:39 +0300

    edit access

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,4 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::resource("/users", \App\Http\Controllers\UserController::class);
-Route::resource("/categories", \App\Http\Controllers\CategoryController::class);
-Route::resource("/tags", \App\Http\Controllers\TagController::class);
-Route::resource("/posts", \App\Http\Controllers\PostController::class);
-
 require __DIR__.'/auth.php';
