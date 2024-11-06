52bc10e6a063bd34e991e7e7e120647191e608f7
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-06 21:59:05 +0200

    using model binding to show post under slug condition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,7 +29,12 @@ class AppServiceProvider extends ServiceProvider
         ]);
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        //be aware that the 'post' variable must be matched (in name) with $post variable in PostController.show [ex : show(App\Models\Post $post)] 
+        Route::bind('post', function ($val) {
+            return Post::where('slug', $val)->firstOrFail();
         });
     }
 }
