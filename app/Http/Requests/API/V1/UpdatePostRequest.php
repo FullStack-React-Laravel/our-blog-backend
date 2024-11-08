ad782dabbe22906463d7d2c7bc9f585736bef364
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-08 22:13:04 +0200

    validating update request

--- app/Http/Requests/API/V1/UpdatePostRequest.php
+++ app/Http/Requests/API/V1/UpdatePostRequest.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\API\V1;
 
+use App\Models\Post;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use function Laravel\Prompts\error;
 
 class UpdatePostRequest extends FormRequest
 {
@@ -11,7 +16,7 @@ class UpdatePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +26,43 @@ class UpdatePostRequest extends FormRequest
      */
     public function rules(): array
     {
+        $post = $this->route('post'); // receives the post parameter from update route  
         return [
-            //
+            // explain the unique rule  : [unique:table , column , ignore ] 
+            //the ignore attribute will make this recored unique comparing with all other records in database except the ignore condition (the record itself in this case)
+            'title' => 'required|min:5|max:50|string|unique:posts,title,' . $post->id,
+            'attachment' => 'sometimes|filled|max:2048|mimes:jpeg,png,jpg,gif,mp4', //2mb
+            'content' => 'required|min:20|string',
+            'category_id' => 'sometimes'
         ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'message' => 'Validation errors',
+            'error' => $validator->errors()
+        ], 422));
+    }
+    public function prepareForValidation()
+    {
+        $newTitle = $this->input('title');
+        $originalTitle = $this->route('post')->title;
+
+        $newTitle !== $originalTitle ? $this->merge(
+            [
+                'slug' => str()->slug($this->input('title'))
+            ]
+        ) : '';
+
+        $newContent = $this->input('content');
+        $originalContent = $this->route('post')->content;
+
+        $newContent !== $originalContent ?
+            $this->merge(
+                [
+                    'excerpt' => substr($this->input('content'), 0, 9),
+                ]
+            ) : '';
     }
 }
