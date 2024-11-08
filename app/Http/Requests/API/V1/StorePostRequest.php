094bf0b82815a777d5b2a9d884b40350a70a9aa3
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-08 22:12:49 +0200

    validating store request

--- app/Http/Requests/API/V1/StorePostRequest.php
+++ app/Http/Requests/API/V1/StorePostRequest.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Requests\API\V1;
 
+
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use Illuminate\Contracts\Validation\Validator;
+
 
 class StorePostRequest extends FormRequest
 {
+    protected $redirect = null;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +28,31 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|min:5|max:50|string|unique:posts,title',
+            'attachment' => 'sometimes|filled|max:2048|mimes:jpeg,png,jpg,gif,mp4', //2mb
+            'content' => 'required|min:20|string',
+            'category_id' => 'required'
         ];
+    }
+    public function prepareForValidation(): void
+    {
+        $this->merge([
+            'slug' => str()->slug($this->title),
+            'excerpt' => substr($this->input('content'), 0, 9),
+            'user_id' => auth()->id(),
+        ]);
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Validation errors',
+                    'errors' => $validator->errors()
+                ],
+                422
+            )
+        );
     }
 }
